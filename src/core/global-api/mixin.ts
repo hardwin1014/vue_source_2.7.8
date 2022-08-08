1224628491cc779d97c96c6cfe3801d46307a9e1
苍南 <914067438@qq.com>
2022-08-08 17:37:28 +0800

    feat: mixin

--- src/core/global-api/mixin.ts
+++ src/core/global-api/mixin.ts
@@ -3,6 +3,8 @@ import { mergeOptions } from '../util/index'
 
 export function initMixin(Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
+    //将用户的选项和全局的options进行合并   注册到全局选项
+    // 把mixin所有的成员拷贝到 options中
     this.options = mergeOptions(this.options, mixin)
     return this
   }
